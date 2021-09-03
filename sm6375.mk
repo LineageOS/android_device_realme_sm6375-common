a997a90bf497d1c075726ca350bbedc409a436c8
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-03 20:26:53 +0300

    sm6375-common: import bootctrl hal and gpt-utils
    Change-Id: If4914a4bd12e709f660b2331ce5ca1252201063b

--- sm6375.mk
+++ sm6375.mk
@@ -83,6 +83,15 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
