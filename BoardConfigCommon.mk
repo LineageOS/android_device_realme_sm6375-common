726d9b34c5e5d676cdde141f970a902fc0d83883
Vivekachooz <vivekachooz@gmail.com>
2023-03-04 12:08:22 +0530

    sm6375-common: Initial makefiles for common tree
    Change-Id: I9c2c50962ee28dbbc34f7562d9778a8cb42f1b97

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/realme/sm6375-common
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo300
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
+
+# Platform
+TARGET_BOARD_PLATFORM := holi
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
---
+++ README.md
@@ -0,0 +1,4 @@
+Copyright (C) 2023 - The LineageOS Project
+
+Common device tree for SM6375 based Realme devices
+================================================
