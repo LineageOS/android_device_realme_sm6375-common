0e0aa03114dc2df71215ef1e7008795d19d3908a
kubersharma001 <kubersharma001@gmail.com>
2023-04-04 22:56:39 +0530

    sm6375-common: Build telephony-ext
    Change-Id: I2dd7d94ba00af4d20aa2410b7c5ee54726a6ed59

--- sm6375.mk
+++ sm6375.mk
@@ -187,6 +187,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
