07e4a5db729293594f221e5276a9dce0d9afe35d
Ramii Ahmed <ramy@ahmedramy.com>
2023-04-04 23:01:03 +0530

    sm6375-common: build QTI Power AIDL
    Change-Id: Ica740c9c7fb02e9f7de59af4c56b1ab35c3f71db

--- sm6375.mk
+++ sm6375.mk
@@ -159,6 +159,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
