02701eb1e05cf40c97d896816a1b4068b75d96c1
Vivekachooz <vivekachooz@gmail.com>
2023-04-14 10:43:30 +0530

    sm6375-common: Build bluetooth and camera hidl
    Change-Id: I6d9c9313967b5fadb2d28ec4acf8caeb6ae10715

--- sm6375.mk
+++ sm6375.mk
@@ -59,7 +59,17 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl
+    android.hardware.bluetooth@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # IPC router config
 PRODUCT_COPY_FILES += \
