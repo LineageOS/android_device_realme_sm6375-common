c56858687357ab2664d77bea750a54e6d294022f
Vivekachooz <vivekachooz@gmail.com>
2023-06-24 17:21:41 +0530

    sm6375-common: Build WiFi Display packages
    Change-Id: I3d59e7adf6e2c359bdaeaccbbc19ec350ddd2ae5

--- sm6375.mk
+++ sm6375.mk
@@ -326,5 +326,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_ODM)/vendor/etc/wifi/WCNSS_qcom_cfg.ini
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/sm6375-common/sm6375-common-vendor.mk)
