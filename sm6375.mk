4b2a4357a550efd4d8112a1c3c8208326aa08376
Vivekachooz <vivekachooz@gmail.com>
2023-06-30 20:17:08 +0530

    sm6375-common: Build necessary non-omx media packages
    Change-Id: Iecfef9027d6c64b61eb5b5fa15f274416804f38e

--- sm6375.mk
+++ sm6375.mk
@@ -234,6 +234,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
+    libcodec2_hidl@1.0.vendor \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
