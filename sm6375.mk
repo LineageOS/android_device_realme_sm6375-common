136215cd3592dc67eabf5a83b504e1a47e223fb8
Vivekachooz <vivekachooz@gmail.com>
2023-08-03 00:03:56 +0530

    sm6375-common: Set PRODUCT_SHIPPING_API_LEVEL to 30
    Change-Id: I00910b444c2a428ddc5b7d99261a9b4d95226531

--- sm6375.mk
+++ sm6375.mk
@@ -387,6 +387,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
