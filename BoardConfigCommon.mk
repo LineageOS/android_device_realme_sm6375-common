079f51a0d71bbbaa68d2ab23ec566d59c25b9be7
Vivekachooz <vivekachooz@gmail.com>
2023-08-22 20:18:46 +0530

    sm6375-common: Build livedisplay, touch hidl and doze from hardware/oplus
    Change-Id: I4e508772f4b5b61258e0a7933e7bbc992dcf4b3c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,6 +73,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
--- sm6375.mk
+++ sm6375.mk
@@ -137,6 +137,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Doze
+PRODUCT_PACKAGES += \
+    OplusDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
@@ -210,6 +214,10 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.oplus
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -436,6 +444,10 @@ PRODUCT_BOOT_JARS += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.oplus
 
 # Update engine
 PRODUCT_PACKAGES += \
