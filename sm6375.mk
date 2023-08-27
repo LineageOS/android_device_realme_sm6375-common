bd711524fda9c58f8a6f0ff66b05ec8e791f1751
Vivekachooz <vivekachooz@gmail.com>
2023-08-27 21:22:36 +0530

    sm6375-common: rootdir: Drop unnecessary rootdir files
    Change-Id: Ia6c4c0db737807e1737d9f9b7cfc4adf27445984

--- sm6375.mk
+++ sm6375.mk
@@ -367,9 +367,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     fstab.default \
-    init.qcom.factory.rc \
     init.qcom.rc \
-    init.qcom.test.rc \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
 
