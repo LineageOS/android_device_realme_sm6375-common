88479158914ddeffd8710a8baab41b4012883189
Vivekachooz <vivekachooz@gmail.com>
2023-08-30 15:55:11 +0530

    sm6375-common: Import initial blobs list
    * From RMX3471_TP1A.220905.001 build
    
    Change-Id: I00cc547e4240ff1c74ae6449318fe8fdb219cb40

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml)
+            sed -i "s/\/my_product/\/product/" "${2}"
+            ;;
+    esac
+}
+
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
