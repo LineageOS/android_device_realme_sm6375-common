8b4a8f037d01a20108ead44d8fc1367cfc5df1d8
Michael Bestas <mkbestas@lineageos.org>
2023-09-28 21:09:01 +0530

    sm6375-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,6 +105,9 @@ BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMO
 BOOT_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.include.recovery))
 TARGET_MODULE_ALIASES += wlan.ko:qca_cld3_wlan.ko
 
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
