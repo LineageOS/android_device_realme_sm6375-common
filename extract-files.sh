ea986bbfc6bd51485400fa549c83cda40291fe34
LuK1337 <priv.luk@gmail.com>
2023-09-29 13:59:00 +0530

    sm6375-common: Use libcamera_metadata_shim for unlocking cameras
    Change-Id: I985c6e317b36d8cdc6b41a9f9f625b293a8987eb

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,9 @@ function blob_fixup() {
         product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml)
             sed -i "s/\/my_product/\/product/" "${2}"
             ;;
+	vendor/lib64/hw/com.qti.chi.override.so)
+            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
+            ;;
     esac
 }
 
