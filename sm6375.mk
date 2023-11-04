4cba5236b0262b1abad9c39307f0f216f201df1f
Vivekachooz <vivekachooz@gmail.com>
2023-11-04 23:12:48 +0530

    sm6375-common: Move variant handling out of common
    Change-Id: I2b44e1f4f2a679a66a953f1b8b16c8ca3bb37699

--- sm6375.mk
+++ sm6375.mk
@@ -389,8 +389,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.default \
     fstab.default.vendor_ramdisk \
-    init.oplus.hw.rc \
-    init.oplus.hw.rc.recovery \
     init.oplus.rc \
     init.qcom.rc \
     init.recovery.qcom.rc \
