8f2eb5ed9bd1f75e45685c745ef0d62ed7c1c143
Vivekachooz <vivekachooz@gmail.com>
2024-01-13 19:33:33 +0530

    sm6375-common: Build android.media.audio.common.types-V1-cpp
    * Needed by wfdservice
    
    Change-Id: I2f3259b6f1255998c88f9cbb909666421bba1768

--- sm6375.mk
+++ sm6375.mk
@@ -249,6 +249,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V1-cpp \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
