af18ec666a2aff7422262b660bb723c8bcc3ffef
LuK1337 <priv.luk@gmail.com>
2024-09-22 17:57:48 +0530

    sm6375-common: Disable ISunlightEnhancement via soong option
    Change-Id: I6d97d679a08492af06603ebacdb1e9c5ee86b4f2

--- sm6375.mk
+++ sm6375.mk
@@ -226,6 +226,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
 
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
