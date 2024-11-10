7a57e29f944093850ff67eba3e5885e07d83a7d3
Vivekachooz <vivekachooz@gmail.com>
2024-11-10 20:57:15 +0530

    sm6375-common: Update from RMX3471_14.0.0.900(EX01)
    Change-Id: I4c00517ecfe90c374cf136a2dcbd703ebb8ed327

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,7 +167,7 @@ include hardware/oplus/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Vendor Security patch level
-BOOT_SECURITY_PATCH := 2024-08-05
+BOOT_SECURITY_PATCH := 2024-11-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Verified Boot
