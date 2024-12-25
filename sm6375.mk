d1c0b25711abece17259e224e46918af91748d78
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm6375-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- sm6375.mk
+++ sm6375.mk
@@ -208,6 +208,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
