d07e02f316423db5ffad8c3dffe3ae0da3dfa87b
Vivekachooz <vivekachooz@gmail.com>
2025-05-16 11:38:27 +0530

    sm6375-common: Update from RMX3660_15.0.0.830(EX01)
    Change-Id: I9c4335edcd6472c7d64fbc58306c5e5882f7acf9

--- extract-files.py
+++ extract-files.py
@@ -49,6 +49,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    'odm/lib64/vendor.oplus.hardware.urcc-V1-ndk_platform.so': blob_fixup()
+        .add_needed('libjsoncpp_shim.so'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
         .regex_replace('/my_product', '/product'),
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
@@ -59,8 +61,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
-    'vendor/etc/media_holi/video_system_specs.json': blob_fixup()
-        .regex_replace('"max_retry_alloc_output_timeout": 2000,', '"max_retry_alloc_output_timeout": 0,'),
+    ('vendor/etc/media_blair/video_system_specs.json', 'vendor/etc/media_holi/video_system_specs.json'): blob_fixup()
+        .regex_replace('"max_retry_alloc_output_timeout": 10000,', '"max_retry_alloc_output_timeout": 0,'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
