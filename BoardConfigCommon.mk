365245c281e5f84ca9e9a6a4d29fc6873127bc89
Vivekachooz <vivekachooz@gmail.com>
2025-08-02 23:31:41 +0530

    sm6375-common: Update from RMX3660_15.0.0.910(EX01)
    Change-Id: Iba8187be77439dc2d7d6d3e2eee52faae5fbe7c2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,7 +162,7 @@ include hardware/oplus/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Vendor Security patch level
-BOOT_SECURITY_PATCH := 2025-04-01
+BOOT_SECURITY_PATCH := 2025-07-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Verified Boot
