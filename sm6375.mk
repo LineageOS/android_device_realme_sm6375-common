33eb08170d2804cef821793e28e3acd40bedeb38
LuK1337 <priv.luk@gmail.com>
2026-03-10 09:12:28 +0100

    sm6375-common: Remove fwk-detect lib from PRODUCT_PACKAGES
    See: Iac75d83e6bc46c137c328a2365e6701c210bfe7a
    Change-Id: I6b8571e81e59f438e34b76823f47ff024ec33dc6

--- sm6375.mk
+++ sm6375.mk
@@ -295,10 +295,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
-# QMI
-PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
-
 # QNS
 PRODUCT_PACKAGES += \
     libstdc++_vendor
