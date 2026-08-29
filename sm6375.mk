8b36bfa9347ea2e8c2f8ddc1092a194757c12f9d
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    sm6375-common: Drop UFFD flags
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- sm6375.mk
+++ sm6375.mk
@@ -177,10 +177,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-OVERRIDE_ENABLE_UFFD_GC := true
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
